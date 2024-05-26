9964c5e64fa4e4e9d1b4327ad2819b0678514f26
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-26 11:14:47 +0100

    added temperature values from API to be shown on page (weather-app-city) and temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let weatherAppCityElement = document.querySelector("#weather-app-city");
+  weatherAppCityElement.innerHTML = response.data.city;
+  console.log(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "94fea4075b9658cte3d8o7c065a17914";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let weatherAppCityElement = document.querySelector("#weather-app-city");
-  weatherAppCityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
