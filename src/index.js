4cdfb880469a7f26b1dd7ebb7b39d61b12635477
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-28 14:06:58 +0100

    added real data from API for weather forecast icon

--- src/index.js
+++ src/index.js
@@ -66,17 +66,16 @@ function displayForecast(response) {
   let forecastHtml = ``;
   response.data.daily.forEach(function (day) {
     forecastHtml += `
-      <div class="weather-forecast-day"> 
-        <div class="weather-forecast-date">Tues</div>
-        <div class="weather-forecast-icon">🌤️</div>
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature-max">
-            <strong>${Math.round(day.temperature.maximum)}º</strong>
-             <span class="weather-forecast-temperature-min">${Math.round(
+  <div class="weather-forecast-day"> 
+     <div  class="weather-forecast-date">Tues</div>
+     <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+         <div class="weather-forecast-temperatures"> 
+       <div class="weather-forecast-temperature-max">
+            <strong>${Math.round(day.temperature.maximum)}º</strong></div>
+             <div class="weather-forecast-temperature-min">${Math.round(
                day.temperature.minimum
-             )}</span>
+             )}  </div>
              </div>
-        </div>
       </div>
     `;
   });
